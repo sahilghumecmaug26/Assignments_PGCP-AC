419236a7011dd21f8d1661de4f6d4d82b552890d
sahilghumecmaug26 <sahil.ghume.cmaug26@gmail.com>
2026-08-28 19:47:34 +0530

    Create Question2.1.cpp

---
+++ CPP/Assignment 1/Question2.1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+// V1 - Call by Value
+void resetSensorPairV1(int reading1, int reading2)
+	{
+	    int temp = reading1;
+	    reading1 = reading2;
+	    reading2 = temp;
+	}
+
+	// V2 - Call by Reference
+	void resetSensorPairV2(int& reading1, int& reading2)
+	{
+	    int temp = reading1;
+	    reading1 = reading2;
+	    reading2 = temp;
+	}
+
+	// V3 - Call by Pointer
+	void resetSensorPairV3(int* reading1, int* reading2)
+	{
+	    int temp = *reading1;
+	    *reading1 = *reading2;
+	    *reading2 = temp;
+	}
+
+
+int main(){
+
+	    int A = 55;
+	    int B = 12;
+
+	    // V1
+	    cout << "V1: Call by Value" << endl;
+	    cout << "Before : A=" << A << "  B=" << B << endl;
+	    resetSensorPairV1(A, B);
+	    cout << "After  : A=" << A << "  B=" << B << endl;
+
+	    // V2
+	    cout << "V2: Call by Reference" << endl;
+	    cout << "Before : A=" << A << "  B=" << B << endl;
+	    resetSensorPairV2(A, B);
+	    cout << "After  : A=" << A << "  B=" << B << endl;
+
+	    // V3
+	    cout << "V3: Call by Pointer" << endl;
+	    cout << "Before : A=" << A << "  B=" << B << endl;
+	    resetSensorPairV3(&A, &B);
+	    cout << "After  : A=" << A << "  B=" << B << endl;
+
+	return 0;
+
+}
