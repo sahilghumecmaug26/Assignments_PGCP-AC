1c1d37bd79e4f9e20d826171d8b6e3c2266d57a4
sahilghumecmaug26 <sahil.ghume.cmaug26@gmail.com>
2026-08-28 19:45:41 +0530

    Create Question1.3.cpp

---
+++ CPP/Assignment 1/Question1.3.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+	double temperature[3][3];
+	cout<<"Enter Temperatures of 9 rooms : "<<endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cin >> temperature[i][j];
+		}
+	}
+
+	int hotestRoom =0 ;
+	int hotestFloor =0 ;
+
+	cout << "\n        Room1   Room2   Room3"<<endl;
+	for(int i=0;i<3;i++){
+		cout << "Floor " << i + 1 << " : ";
+		for(int j=0;j<3;j++){
+			cout << temperature[i][j] << "     ";
+		}
+		cout << endl;
+	}
+
+    double hottest = temperature[0][0];
+    int hottestFloor = 0;
+    int hottestRoom = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (temperature[i][j] > hottest)
+            {
+                hottest = temperature[i][j];
+                hottestFloor = i;
+                hottestRoom = j;
+            }
+        }
+    }
+    cout << "Hottest Room  : Floor "<< hottestFloor + 1<< ", Room " << hottestRoom + 1<< "  ->  " << hottest << "°C" << endl;
+
+    double highestAverage = 0;
+    int hottestFloorNumber = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        double sum = 0;
+
+        for (int j = 0; j < 3; j++)
+        {
+            sum += temperature[i][j];
+        }
+
+        double average = sum / 3;
+
+        if (average > highestAverage)
+        {
+            highestAverage = average;
+            hottestFloorNumber = i;
+        }
+    }
+
+    cout << "Hottest Floor : Floor "<< hottestFloorNumber + 1 << "  (avg " << highestAverage << "°C)" << endl;
+
+    int warningRooms = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (temperature[i][j] >= 30)
+            {
+                warningRooms++;
+            }
+        }
+    }
+
+    cout << "Rooms at WARNING or above : "<< warningRooms << endl;
+
+	return 0;
+}
+
