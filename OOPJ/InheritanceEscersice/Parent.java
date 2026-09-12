c3905207b02caa1f20ed760e21f6655519b26bb0
sahilghumecmaug26 <sahil.ghume.cmaug26@gmail.com>
2026-09-12 16:35:14 +0530

    Create Parent.java

---
+++ OOPJ/InheritanceEscersice/Parent.java
@@ -0,0 +1,18 @@
+
+// Question 1 
+
+public class Parent {
+	void parentMethod() {
+		System.out.println("This is a parent class");
+		
+	}
+}
+
+class Child extends Parent {
+	
+	void chaildMethod() {
+		System.out.println("This is a child class  ");
+	}
+	
+	
+}
