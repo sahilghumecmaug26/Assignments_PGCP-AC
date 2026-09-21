dc5eca9f645f1354e77b14dd6552e6da24d87079
sahilghumecmaug26 <sahil.ghume.cmaug26@gmail.com>
2026-09-21 12:43:07 +0530

    Create Problem6.java

---
+++ OOPJ/StringExercise/Problem6.java
@@ -0,0 +1,12 @@
+class program{
+  public static void main(String[] args){
+        String str = "The Quick BroWn FoX!";
+
+        System.out.println("Original String: " + str);
+
+        String upperCase = str.toUpperCase();
+
+        System.out.println("String in uppercase: " + upperCase);
+
+  }
+}
