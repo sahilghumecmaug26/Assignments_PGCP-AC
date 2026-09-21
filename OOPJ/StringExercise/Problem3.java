9fc22b9a096756c1512e80455d2fa43dfbde097f
sahilghumecmaug26 <sahil.ghume.cmaug26@gmail.com>
2026-09-21 12:25:58 +0530

    Create Problem3.java

---
+++ OOPJ/StringExercise/Problem3.java
@@ -0,0 +1,14 @@
+class Program{
+  public static void main(String [] args){
+          String str1 = "Python Exercises";
+	        String str2 = "se";
+
+	        System.out.println("\"" + str1 + "\" ends with \"" + str2 + "\"? "
+	                + str1.endsWith(str2));
+
+	        str1 = "Python Exercise";
+
+	        System.out.println("\"" + str1 + "\" ends with \"" + str2 + "\"? "
+	                + str1.endsWith(str2));
+  }
+}
