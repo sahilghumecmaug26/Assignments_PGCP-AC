b169a8a8ca8268b0274c83e4da9766ae8d3ffc41
sahilghumecmaug26 <sahil.ghume.cmaug26@gmail.com>
2026-09-21 12:47:09 +0530

    Create EquilateralTriangle.java

---
+++ OOPJ/InterfaceExercise/EquilateralTriangle.java
@@ -0,0 +1,22 @@
+
+public class EquilateralTriangle implements RegularPolygon {
+
+	private double sideLength;
+	
+	public EquilateralTriangle(double sideLength) {
+		this.sideLength = sideLength;
+	}
+	
+	
+	@Override
+	public int getNumSides() {
+	
+		return 3;
+	}
+
+	@Override
+	public double getSideLength() {
+		return sideLength;
+	}
+
+}
